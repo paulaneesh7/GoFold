5dcb40a85f7d9a5318bf7a6b4e2e215f6ebd18e5
Aneesh Paul <aneesh16117@gmail.com>
2024-08-09 22:14:44 +0530

    Struct pointer

--- go_max2/struct_init/structs.go
+++ go_max2/struct_init/structs.go
@@ -13,6 +13,20 @@ type User struct {
 	createdAt time.Time
 }
 
+/* Attaching the function to the struct, so that's why (u User) at the begining */
+func (u User) outputDetails(){
+	fmt.Println("First Name: ", u.firstName, "Last Name: ", u.lastName, "Birth Date: " , u.birthdate)
+}
+
+
+/* We have used pointer here because otherwise it doesn't change the main-User struct's fields
+It's creating a copy of its own and changing the fields of that copy */
+func (u *User) clearUserName(){
+	u.firstName = ""
+	u.lastName = ""
+}
+
+
 func StructInitial() {
 	userFirstName := getUserData("Please enter your first name: ")
 	userLastName := getUserData("Please enter your last name: ")
@@ -26,15 +40,16 @@ func StructInitial() {
 		createdAt: time.Now(),
 	}
 
-	// ... do something awesome with that gathered data!
+	
+	// ... do something awesome with that gathered data, for practse purpose we are using pointers here
+	appUser.outputDetails()
 
-	outputDetails(appUser)
+	// changes the data in the struct
+	appUser.clearUserName()
+	appUser.outputDetails()
 }
 
-/*Just like how we define params (name int) like that (u User) or (user User)*/
-func outputDetails(u User){
-	fmt.Println("First Name: ", u.firstName, "Last Name: ", u.lastName, "Birth Date: " , u.birthdate)
-}
+
 
 func getUserData(promptText string) string {
 	fmt.Print(promptText)
