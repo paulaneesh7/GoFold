9c07d3a21b02ede160e225722883c7ea0e1a6ae9
Aneesh Paul <aneesh16117@gmail.com>
2024-09-07 12:06:22 +0530

    Interface and Embedded Interfaces

--- go_max3/todo/todo.go
+++ go_max3/todo/todo.go
@@ -3,13 +3,27 @@ package todo
 import (
 	"bufio"
 	"fmt"
+	"go_max3/note"
 	todostruct "go_max3/todo_struct"
 	"os"
 	"strings"
 )
 
+// Any struct that implements the saver interface must have a Saver method and Returns an error.
+type saver interface{
+	Save() error
+}
+
+
+// Embedding saver interface and Display method together in the outputtable interface.
+type outputtable interface{
+	saver
+	Display()
+}
+
+
 func CreateTodo() {
-	content := getTodoData()
+	title, content := getNoteData()
 
 	todo, err := todostruct.New(content)
 
@@ -18,21 +32,48 @@ func CreateTodo() {
 		return
 	}
 
-	todo.Display()
-
-	err = todo.Save()
+	userNote, err := note.New(title, content)
 
 	if err != nil {
-		fmt.Println("Error saving todo:", err)
+		fmt.Println(err)
 		return
 	}
+
+	err = outputData(todo)
+
+	if err != nil {
+		return
+	}
+
+	outputData(userNote)
+
 }
 
-func getTodoData() string {
 
-	content := getUserInput("Enter Todo content: ")
+func getNoteData() (string, string) {
+	title := getUserInput("Note title:")
+	content := getUserInput("Note content:")
 
-	return content
+	return title, content
+}
+
+// Using the outputtable interface as a type to "data" parameter
+func outputData(data outputtable) error {
+	data.Display()
+	return saveData(data)
+}
+
+// Using the saver interface as a type to "data" parameter
+func saveData(data saver) error {
+	err := data.Save()
+
+	if err != nil {
+		fmt.Println("Saving the note failed.")
+		return err
+	}
+
+	fmt.Println("Saving the note succeeded!")
+	return nil
 }
 
 func getUserInput(prompt string) string {
