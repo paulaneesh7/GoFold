0388c3c45282676cbf761070701b4b32622c25d3
Aneesh Paul <aneesh16117@gmail.com>
2025-01-18 01:21:35 +0530

    Tax calculator basics start

--- tax_calculator/main.go
+++ tax_calculator/main.go
@@ -1,12 +1,19 @@
 package main
 
+import (
+	"fmt"
 
-
-import "fmt"
-
+	"github.com/paulaneesh7/tax_calculator/prices"
+)
 
 func main() {
 	fmt.Println("Price Tax Calculator")
 
-	
+	taxRate := []float64{0.0, 0.07, 0.1, 0.15}
+
+	for _, taxRate := range taxRate {
+		priceJobs := prices.NewTaxIncludedPriceJob(taxRate)
+		priceJobs.Process()
+	}
+
 }
