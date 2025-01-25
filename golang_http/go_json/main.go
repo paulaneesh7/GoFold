e843bd89c4ad0234bb54f02898804dbc1af3220a
Aneesh Paul <aneesh16117@gmail.com>
2025-01-25 20:37:33 +0530

    Go server

--- golang_http/go_json/main.go
+++ golang_http/go_json/main.go
@@ -1,10 +1,67 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"net/http"
 )
 
+type course struct {
+	Name     string `json:"course_name"`
+	Price    int64
+	Platform string
+	Password string
+	Tags     []string
+}
+
 func main() {
-	fmt.Println("Http Server In Golang")
+	fmt.Println("Creating JSON In Golang")
+
+	EncodeJSON()
+	DecoderJSON()
+}
+
+// Converting JSON to a suitable format
+func EncodeJSON() {
+	kzCourses := []course{
+		{"React", 299, "Udemy", "abc", []string{"web-dev", "js"}},
+		{"Angular", 199, "Udemy", "abc", []string{"web-dev", "js"}},
+		{"Golang", 199, "LCO", "abc", nil},
+	}
+
+	// package this data as JSON data
+	finalJSON, err := json.Marshal(kzCourses)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(finalJSON))
+}
+
+
+func DecoderJSON() {
+
+	// Any data that comes from the web comes in byte[] format
+	jsonDataFromWeb := []byte(
+		`{"course_name":"React", "price":299, "platform":"Udemy", "password":"abc", "tags":["web-dev", "js"]}`,
+	)
+
+	var kzCourse course
+
+	// Check whether the data is valid JSON (it doesn't throw an error it just gives a bool)
+	checkValid := json.Valid(jsonDataFromWeb)
+	if checkValid {
+		fmt.Println("JSON is valid",checkValid)
+		json.Unmarshal(jsonDataFromWeb, &kzCourse)
+		fmt.Println(kzCourse)
+	} else {
+		fmt.Println("JSON is not valid")
+	}
+
+	// some cases where you might want to add data to key-value pair
+	// ( here we took interface for value because we don't know the type of value )
+	var myOnlineData map[string]interface{}
+
+	json.Unmarshal(jsonDataFromWeb, &myOnlineData)
+	for key, value := range myOnlineData {
+		fmt.Printf("Key is %s and value is %v\n", key, value)
+	}
 }
--- golang_http/go_server/main.go
+++ golang_http/go_server/main.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 )
+
+// Model for courses - file
+
+type Course struct {
+	CourseId    string `json:"courseid"`
+	CourseName  string `json:"coursename"`
+	CoursePrice int    `json:"price"`
+	Author      *Author `json:"author"` // we specified a pointer here because we don't want to create a copy of the Author: Firstname Lastname when we add data to it later
+}
+
+type Author struct {
+	Fullname string `json:"fullname"`
+	Website  string `json:"website"`
+}
+
+
+// fake DB
+var course []Course
+
+
+// middleware - file
+func (c *Course) IsEmpty() bool {
+	return c.CourseId == "" && c.CourseName == "" && c.CoursePrice == 0 && c.Author == nil
+}
 
 func main() {
 	fmt.Println("Http Server In Golang")
