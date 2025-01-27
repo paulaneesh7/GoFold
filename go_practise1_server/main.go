cf24e7e504986407c013b6e2d9dba9580c234e99
Aneesh Paul <aneesh16117@gmail.com>
2025-01-27 20:17:21 +0530

    Products API structs, POST, PUT and DELETE route and controllers

--- go_practise1_server/main.go
+++ go_practise1_server/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -27,6 +28,10 @@ type Manufacturer struct {
 	ManufacturingCountry string `json:"manufacturing_country"`
 }
 
+func (p *Product) IsEmpty() bool {
+	return p.ProductId == "" && p.ProductName == "" && p.Price == 0 && p.Category == nil && p.Manufacturer == nil
+}
+
 
 var products []Product
 
@@ -35,9 +40,13 @@ func main() {
 
 	r := mux.NewRouter();
 
+	r.HandleFunc("/api/products", CreateProduct).Methods("POST")
 	r.HandleFunc("/", HomeRoute).Methods("GET")
 	r.HandleFunc("/api/products", GetAllProducts).Methods("GET")
 	r.HandleFunc("/api/products/{id}", GetProductById).Methods("GET")
+	r.HandleFunc("/api/products/{id}", UpdateProductById).Methods("PUT")
+	r.HandleFunc("/api/products/{id}", DeleteProductById).Methods("DELETE")
+	
 
 
 	// seed some dummy data into the products slice
@@ -80,3 +89,84 @@ func GetProductById(w http.ResponseWriter, r *http.Request) {
 
 	json.NewEncoder(w).Encode(fmt.Sprintf("No product found with id %v", id))
 }
+
+
+func CreateProduct(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Create a Product")
+	w.Header().Set("Content-Type", "application/json")
+	
+	var product Product
+	
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("Please provide input")
+		return 
+	}
+
+	json.NewDecoder(r.Body).Decode(&product)
+
+	if product.IsEmpty() {
+		json.NewEncoder(w).Encode("No data found in the body")
+		return
+	}
+
+	// generate a unique id (for product) and convert it into string (just generated it through autogeneration)
+	product.ProductId = strconv.Itoa(len(products) + 1)
+
+	// append product into products
+	products = append(products, product)
+
+	json.NewEncoder(w).Encode(product)
+}
+
+
+func UpdateProductById(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Update a Product by Id")
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+	id := params["id"]
+
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("Please provide input")
+		return
+	}
+
+	var updatedProduct Product
+	_ = json.NewDecoder(r.Body).Decode(&updatedProduct)
+
+	for index, p := range products {
+		if p.ProductId == id {
+			products[index].ProductId = id
+			products[index].ProductName = updatedProduct.ProductName
+			products[index].Price = updatedProduct.Price
+			products[index].Category = updatedProduct.Category
+			products[index].Manufacturer = updatedProduct.Manufacturer
+			json.NewEncoder(w).Encode(products[index])
+			return
+		}
+	}
+
+
+	json.NewEncoder(w).Encode(fmt.Sprintf("No product found with id %v", id))
+}
+
+
+func DeleteProductById(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Delete a Product by Id")
+	w.Header().Set("Content-Type", "applcation/json")
+
+	params := mux.Vars(r)
+	id := params["id"]
+
+	for index, p := range products {
+		if p.ProductId == id {
+			products = append(products[:index], products[index+1:]...)
+			json.NewEncoder(w).Encode("Product deleted successfully")
+			return
+		}
+	}
+
+	json.NewEncoder(w).Encode(fmt.Sprintf("No product found with id %v", id))
+}
+
+
